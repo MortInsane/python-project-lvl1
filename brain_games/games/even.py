19564f35fde6740142b5e9c78fe71057efcb5a06
Mort Insane <mort.insane@gmail.com>
2022-09-01 19:53:58 +0300

    refactor brain-even game

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,23 +1,19 @@
-from brain_games.game_engine import run_game
 from random import randint
 
 
 GAME_CONDITION = 'Answer "yes" if the number is even, otherwise answer "no".'
+START = 1
+STOP = 100
 
 
 def even_logic():
-    NUMBER = randint(1, 100)
-    yes_answer, no_answer = 'yes', 'no'
+    NUMBER = randint(START, STOP)
 
     if NUMBER % 2 == 0:
-        success_answer = yes_answer
+        success_answer = 'yes'
     else:
-        success_answer = no_answer
+        success_answer = 'no'
 
     question_print = str(NUMBER)
 
     return success_answer, question_print
-
-
-def run_game_even():
-    run_game(game_condition=GAME_CONDITION, game=even_logic)
