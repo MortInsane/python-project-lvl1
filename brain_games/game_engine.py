9082fc5fa2c9ea8694a99a3d123403c097e06653
Mort Insane <mort.insane@gmail.com>
2022-09-07 23:31:29 +0300

    refactor game_engine

--- brain_games/game_engine.py
+++ brain_games/game_engine.py
@@ -1,43 +1,31 @@
 import prompt
 
 
-def welcome_user(game_condition):
+def run_game(game):
+    count_success_answer = 0
+
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
-    print(game_condition)
-    return name
-
-
-def game_checker(question, success_answer):
-    print(f'Question: {question}')
-    user_answer = prompt.string('Your answer: ')
-
-    if user_answer == success_answer:
-        return 'correct'
-    else:
-        return user_answer
-
-
-def run_game(game_condition, game):
-    count_success_answer = 0
-    name = welcome_user(game_condition)
 
     while True:
-        success_answer, question_print = game()
+        answer, question, game_rule = game()
+
+        if count_success_answer == 0:
+            print(game_rule)
 
         if count_success_answer == 3:
             print(f'Congratulations, {name}!')
             return
 
-        attempt = game_checker(question=question_print,
-                               success_answer=success_answer)
+        print(f'Question: {question}')
+        attempt = prompt.string('Your answer: ')
 
-        if attempt == 'correct':
+        if attempt == answer:
             print('Correct!')
             count_success_answer += 1
         else:
             print(f'\'{attempt}\' is wrong answer ;(. '
-                  f'Correct answer was \'{success_answer}\'\n'
+                  f'Correct answer was \'{answer}\'\n'
                   f'Let\'s try again, {name}!')
             return
