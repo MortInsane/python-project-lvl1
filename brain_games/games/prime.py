4b202fc3488535675994b46ddb26feb0adbd10ca
Mort Insane <mort.insane@gmail.com>
2022-09-07 23:31:29 +0300

    refactor brain_prime

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,7 +1,7 @@
 from random import randint
 
 
-GAME_CONDITION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+GAME_RULE = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 START = 1
 STOP = 100
 
@@ -16,14 +16,14 @@ def is_prime(a):
     return a == d
 
 
-def prime_logic():
-    NUMBER = randint(START, STOP)
+def game_logic():
+    number = randint(START, STOP)
 
-    if is_prime(NUMBER):
-        success_answer = 'yes'
+    if is_prime(number):
+        answer = 'yes'
     else:
-        success_answer = 'no'
+        answer = 'no'
 
-    question_print = str(NUMBER)
+    question = str(number)
 
-    return success_answer, question_print
+    return answer, question, GAME_RULE
--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
-from brain_games.games.prime import GAME_CONDITION, prime_logic
+from brain_games.games.prime import game_logic
 from brain_games.game_engine import run_game
 
 
 def main():
-    run_game(game_condition=GAME_CONDITION, game=prime_logic)
+    run_game(ame=game_logic)
 
 
 if __name__ == '__main__':
