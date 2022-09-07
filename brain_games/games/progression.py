1db58cee6f717a502437ec50250c13278284ec51
Mort Insane <mort.insane@gmail.com>
2022-09-07 23:31:30 +0300

    refactor brain_progression

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -1,14 +1,14 @@
 from random import randint, choice
 
 
-GAME_CONDITION = 'What number is missing in the progression?'
+GAME_RULE = 'What number is missing in the progression?'
 OFFSETS = [2, 3, 4, 5]
 START_RANGE = 1
 STOP_RANGE = 100
 NUMBER = 10
 
 
-def draw_progression():
+def print_progression():
     offset = choice(OFFSETS)
     start = randint(START_RANGE, STOP_RANGE)
     end = start + (NUMBER * offset)
@@ -16,21 +16,21 @@ def draw_progression():
     progression = ''
     replaced_index = choice(range(NUMBER))
     replaced_num = 0
+
     for index, num in enumerate(range(start, end, offset)):
         if index == replaced_index:
             progression += '.. '
             replaced_num = num
         else:
             progression += f'{str(num)} '
+
     progression = progression.strip()
+    replaced_num = str(replaced_num)
 
     return progression, replaced_num
 
 
-def progression_logic():
-    question, success_answer = draw_progression()
-    success_answer = str(success_answer)
+def game_logic():
+    question, answer = print_progression()
 
-    question_print = question
-
-    return success_answer, question_print
+    return answer, question, GAME_RULE
