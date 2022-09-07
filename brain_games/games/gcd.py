1c0821b6bec2e04d2e4096442f62402962e5dc73
Mort Insane <mort.insane@gmail.com>
2022-09-07 23:31:29 +0300

    refactor brain_gcd

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -2,17 +2,17 @@ from random import randint
 from math import gcd
 
 
-GAME_CONDITION = 'Find the greatest common divisor of given numbers.'
+GAME_RULE = 'Find the greatest common divisor of given numbers.'
 START = 1
 STOP = 100
 
 
-def gcd_logic():
+def game_logic():
 
-    NUMBER1 = randint(START, STOP)
-    NUMBER2 = randint(START, STOP)
+    number1 = randint(START, STOP)
+    number2 = randint(START, STOP)
 
-    success_answer = str(gcd(NUMBER1, NUMBER2))
-    question_print = f'{NUMBER1} {NUMBER2}'
+    answer = str(gcd(number1, number2))
+    question = f'{number1} {number2}'
 
-    return success_answer, question_print
+    return answer, question, GAME_RULE
--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
-from brain_games.games.gcd import GAME_CONDITION, gcd_logic
+from brain_games.games.gcd import game_logic
 from brain_games.game_engine import run_game
 
 
 def main():
-    run_game(game_condition=GAME_CONDITION, game=gcd_logic)
+    run_game(game=game_logic)
 
 
 if __name__ == '__main__':
