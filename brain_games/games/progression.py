c846712c0208ea52331cff0378cd83edab27d96d
Mort Insane <mort.insane@gmail.com>
2022-09-18 03:00:03 +0300

    refactor brain_progression

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -1,23 +1,26 @@
-from random import randint, choice
+from random import randint
 
 
 GAME_RULE = 'What number is missing in the progression?'
-OFFSETS = [2, 3, 4, 5]
+MIN_STEP = 2
+MAX_STEP = 10
 START_RANGE = 1
 STOP_RANGE = 100
 LENGHT_PROGRESSION = 10
 
 
-def launch_game():
-    offset = choice(OFFSETS)
-    start = randint(START_RANGE, STOP_RANGE)
-    end = start + (LENGHT_PROGRESSION * offset)
+def generate_round():
+    step_progression = randint(MIN_STEP, MAX_STEP)
+    start_progression = randint(START_RANGE, STOP_RANGE)
+    end_progression = start_progression + LENGHT_PROGRESSION * step_progression
 
     question = ''
-    replaced_index = choice(range(LENGHT_PROGRESSION))
+    replaced_index = randint(START_RANGE - 1, LENGHT_PROGRESSION - 1)
     answer = 0
 
-    for index, num in enumerate(range(start, end, offset)):
+    for index, num in enumerate(range(start_progression,
+                                      end_progression,
+                                      step_progression)):
         if index == replaced_index:
             question += '.. '
             answer = num
--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
 from brain_games.games import progression
-from brain_games.game_engine import launch_engine
+from brain_games.game_engine import launch_game
 
 
 def main():
-    launch_engine(progression)
+    launch_game(progression)
 
 
 if __name__ == '__main__':
