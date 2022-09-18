7d59e8c75c019bbe8548c0a7797e6f588cb3b5fc
Mort Insane <mort.insane@gmail.com>
2022-09-18 03:00:03 +0300

    refactor game_engine

--- brain_games/game_engine.py
+++ brain_games/game_engine.py
@@ -1,11 +1,11 @@
 import prompt
 
 
-ROUND_COUNT = 3
+ROUNDS_COUNT = 3
 
 
-def launch_engine(game):
-    count_success_answer = 0
+def launch_game(game):
+    success_answers_count = 0
 
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
@@ -13,9 +13,9 @@ def launch_engine(game):
     print(game.GAME_RULE)
 
     while True:
-        answer, question = game.launch_game()
+        answer, question = game.generate_round()
 
-        if count_success_answer == ROUND_COUNT:
+        if success_answers_count == ROUNDS_COUNT:
             print(f'Congratulations, {name}!')
             return
 
@@ -24,7 +24,7 @@ def launch_engine(game):
 
         if user_answer == answer:
             print('Correct!')
-            count_success_answer += 1
+            success_answers_count += 1
         else:
             print(f'\'{user_answer}\' is wrong answer ;(. '
                   f'Correct answer was \'{answer}\'\n'
