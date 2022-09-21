621763cc632f6ba3df9c031bd8162915a5db510c
Mort Insane <mort.insane@gmail.com>
2022-09-21 03:52:47 +0300

    add func generate_progression

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -9,20 +9,28 @@ STOP_RANGE = 100
 LENGHT_PROGRESSION = 10
 
 
+def generate_progression(start, end, step, lenght):
+    progression = []
+
+    for num in range(start, end, step):
+        progression.append(str(num))
+
+    replaced_index = randint(0, lenght - 1)
+    answer = str(progression[replaced_index])
+    progression[replaced_index] = '..'
+    progression = ' '.join(progression)
+
+    return answer, progression
+
+
 def generate_round():
-    step_progression = randint(MIN_STEP, MAX_STEP)
     start_progression = randint(START_RANGE, STOP_RANGE)
+    step_progression = randint(MIN_STEP, MAX_STEP)
     end_progression = start_progression + LENGHT_PROGRESSION * step_progression
 
-    question = []
-
-    for num in range(start_progression, end_progression, step_progression):
-        question.append(str(num))
-
-    replaced_index = randint(0, LENGHT_PROGRESSION - 1)
-    answer = str(question[replaced_index])
-    question[replaced_index] = '..'
-
-    question = ' '.join(question)
+    answer, question = generate_progression(start_progression,
+                                            end_progression,
+                                            step_progression,
+                                            LENGHT_PROGRESSION)
 
     return answer, question
