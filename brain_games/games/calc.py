57c1a5e39ede98b9752f468123ab37cddb0b285e
Mort Insane <mort.insane@gmail.com>
2022-09-21 03:25:45 +0300

    add func get_expression

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -7,21 +7,24 @@ STOP_RANGE = 100
 OPERATORS = ['+', '-', '*']
 
 
+def get_expression(num1, num2, operator):
+    answer = 0
+    if operator == '-':
+        answer = num1 - num2
+    elif operator == '+':
+        answer = num1 + num2
+    elif operator == '*':
+        answer = num1 * num2
+
+    return str(answer)
+
+
 def generate_round():
     number1 = randint(START_RANGE, STOP_RANGE)
     number2 = randint(START_RANGE, STOP_RANGE)
-
     operator = choice(OPERATORS)
 
-    answer = 0
-    if operator == '-':
-        answer = number1 - number2
-    elif operator == '+':
-        answer = number1 + number2
-    elif operator == '*':
-        answer = number1 * number2
-
-    answer = str(answer)
+    answer = get_expression(number1, number2, operator)
     question = f'{number1} {operator} {number2}'
 
     return answer, question
