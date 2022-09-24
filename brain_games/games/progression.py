4ac7e120dc7cf1034fb46826387830ce00b3d441
Mort Insane <mort.insane@gmail.com>
2022-09-24 04:11:56 +0300

    change func generate_progression & rename vars

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -6,16 +6,15 @@ MIN_STEP = 2
 MAX_STEP = 10
 START_RANGE = 1
 STOP_RANGE = 100
-LENGHT_PROGRESSION = 10
+LENGHT = 10
 
 
-def generate_progression(start, end, step, lenght):
+def generate_progression(start, end, step, replaced_index):
     progression = []
 
     for num in range(start, end, step):
         progression.append(str(num))
 
-    replaced_index = randint(0, lenght - 1)
     answer = str(progression[replaced_index])
     progression[replaced_index] = '..'
     progression = ' '.join(progression)
@@ -26,11 +25,12 @@ def generate_progression(start, end, step, lenght):
 def generate_round():
     start_progression = randint(START_RANGE, STOP_RANGE)
     step_progression = randint(MIN_STEP, MAX_STEP)
-    end_progression = start_progression + LENGHT_PROGRESSION * step_progression
+    end_progression = start_progression + LENGHT * step_progression
+    replaced_index = randint(0, LENGHT - 1)
 
     answer, question = generate_progression(start_progression,
                                             end_progression,
                                             step_progression,
-                                            LENGHT_PROGRESSION)
+                                            replaced_index)
 
     return answer, question
